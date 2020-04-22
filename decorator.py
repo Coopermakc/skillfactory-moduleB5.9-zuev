d467cf4c05c0094b40f4db014e089938a88fe74a
Coopermakc <Coopermakc@mail.ru>
2020-04-22 18:04:49 +0300

    decorator init

---
+++ decorator.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 17:57:11 2020
+
+@author: macbook
+"""
+import time
+from contextlib import ContextDecorator
+
+#класс декоратора с возможностью использования как контекстного менеджера
+class Timer(ContextDecorator):
+    
+    def __init__(self,num_runs):
+        self.num_runs = num_runs
+        
+    def __call__(self,func):
+        def inner_function(args):
+            avg_time = 0
+            for _ in range(self.num_runs):
+                t0=time.time()
+                func(args)
+                t1=time.time()
+                avg_time += (t1-t0)
+                avg_time /= self.num_runs
+            return "Time %.5f" %avg_time
+        return inner_function
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, *args):
+        return self
+    
+#обычный декоратор
+def time_this(num_runs):
+    def decorator(func):
+        def wrap(param):
+            avg_time = 0
+            for _ in range(num_runs):
+                t0=time.time()
+                func(param)
+                t1=time.time()
+                avg_time += (t1-t0)
+            avg_time /= num_runs
+            return "Time %.5f" %avg_time
+        return wrap
+    return decorator
